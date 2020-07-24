175b38570be99dedf4c1c4e7b19f56a671a30fac
Amirhosein <gooyacoder@gmail.com>
2020-07-24 12:53:39 +0430

    4_23

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 def home(request):
-	return render(request, 'home.html', {})
+
+	if request.method == 'POST':
+		form = ListForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			items = List.objects.all
+			messages.success(request, ('Item Added Successfully!'))
+			return render(request, 'home.html', {'items' : items})
+
+	else:
+		items = List.objects.all
+		return render(request, 'home.html', {'items' : items})
+
 
 def about(request):
 	context = {'greeting' : 'Good Morning!', 'app' : 'To-Do List Application'}
